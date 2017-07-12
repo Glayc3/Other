5b74373156faf3c8b3c80153365752c6d5205588
Glayce <Glayc3@users.noreply.github.com>
2017-07-12 17:33:25 +0100

    Update dewi.py

--- dewi.py
+++ dewi.py
@@ -1,37 +1,37 @@
 from time import sleep
-def speech(text,time):    
-    print(text)    
+def speech(text,time):    
+    print(text)    
     sleep(time)
-print("???: hey you")
-sleep(0.5)
-print("Dewi: me name is dewi")
+speech("???: hey you",0.3)
+speech("Dewi: me name is dewi",0.7)
 while True:
-    x = input("Dewi: u lad or lass m999999999999999??????")    
-    if x.lower() == "lass":        
-        gender = f        
-        speech("Dewi: i like wee little girls like you",1)        
-        speech("Dewi: especially when you squeal for help once i take you into the dungeon",1)    
-        elif x.lower() == "lad":        
-            gender = m        
-            speech("Dewi: dammit, wrong half of the population m9",1)        
-            speech("Dewi: do me a favor and kys",0.5)        
-            x2 = input("will you kill yourself?")        
-            if x2.lower() == "yes":            
-                print("GAME OVER SON")            
-                break        
-            elif x2.lower() == "no":            
-                speech("Dewi: eeh what mate",0.5)            
-                speech("i'll help you",0.25)            
-                speech("just jump in here noose",0.5)            
-                x3 = input("jump in the noose?")            
-                if x3.lower() == "yes":                
-                    print("GAME OVER SON")                
-                    break           
-                elif x3.lower() == "no":                
-                    speech("Dewi: Damn you're persitent",0.5)            
-                else:                
-                    speech("this is serious! say it properly",0.3)        
-            else:            
-                speech("this is serious! say it properly",0.3)    
-         else:        speech("what did you say???",0.5)        
-            speech("louder mate",0.3)
+    x = input("Dewi: u lad or lass m999999999999999??????")    
+    if x.lower() == "lass":        
+        gender = "f"        
+        speech("Dewi: i like wee little girls like you",1)        
+        speech("Dewi: especially when you squeal for help once i take you into the dungeon",1)    
+    elif x.lower() == "lad":        
+        gender = "m"        
+        speech("Dewi: dammit, wrong half of the population m9",1)        
+        speech("Dewi: do me a favor and kys",0.5)        
+        x2 = input("will you kill yourself?")        
+        if x2.lower() == "yes":            
+            print("GAME OVER SON")            
+            break        
+        elif x2.lower() == "no":            
+            speech("Dewi: eeh what mate",0.5)            
+            speech("i'll help you",0.25)            
+            speech("just jump in here noose",0.5)            
+            x3 = input("jump in the noose?")            
+            if x3.lower() == "yes":                
+                print("GAME OVER SON")                
+                break           
+            elif x3.lower() == "no":                
+                speech("Dewi: Damn you're persitent",0.5)            
+            else:                
+                speech("this is serious! say it properly",0.3)        
+        else:            
+            speech("this is serious! say it properly",0.3)    
+    else:
+        speech("what did you say???",0.5)        
+        speech("louder mate",0.3)
