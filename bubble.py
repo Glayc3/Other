d7146d4289a8523935774306a6efa5af756be55f
Glayce <Glayc3@users.noreply.github.com>
2017-07-16 18:36:15 +0100

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,15 +1,16 @@
 x=[]
 while True:
-	try:
-        x1 = int(input("how long the list?"))
+    try:
+        x1 = int(input("how long the list (2<=ans>=30)?"))
         if x1<2 or x1>30:
             print("invalid number")
         else:
             break
-    except ValueError:
+    except TypeError:
         print("not a number")
+
 for i in range(x1):
-    x.append(int(input("enter number no "+ i)))
+    x.append(int(input("enter number no "+ str(i+1))))
 def bubbleSort(alist):
     for passnum in range(len(alist)-1,0,-1):
         for i in range(passnum):
