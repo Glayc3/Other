c45d07547e3b819ebc5d82fb493cc56ba63cb548
Glayce <Glayc3@users.noreply.github.com>
2017-11-30 16:06:38 +0000

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,13 +1,15 @@
+from time import sleep
 for i in range(123456789):
     for r in range(8):
         x = (r+1)*i
         t = [int(y) for y in str(i)]
         x2 = [int(e) for e in str(x)]
-        if t in x:
+        if t in x2:
             print(i)
             print("YES")
+            break
         else:
             pass
-
-
-        
+    print(i)
+    print("NO")
+    sleep(0.05)
