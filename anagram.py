1333f741fea2db2b88cb51b3889deb5ed3651a07
Glayce <Glayc3@users.noreply.github.com>
2018-01-18 11:20:48 +0000

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,4 +1,6 @@
 from time import sleep
+no_of_yes = 0
+
 for i in range(123456789):
     for r in range(8):
         x = (r+1)*i
@@ -6,9 +8,11 @@ for i in range(123456789):
         x2 = [int(e) for e in str(x)]
         if t in x2:
             print(i)
-            print("YES")
-            break
+            print("YES " + str((r+1)))
+            no_of_yes += 1
+            continue
         else:
             pass
     print(i)
     print("NO")
+print(str(no_of_yes))
