3b5bc4e593c5d189f04d002702224d42f2e79a68
Glayce <Glayc3@users.noreply.github.com>
2018-02-08 11:36:20 +0000

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,4 +1,5 @@
 x=[]
+
 while True:
     try:
         x1 = int(input("how long the list (2<=ans>=30)?"))
@@ -11,12 +12,19 @@ while True:
 
 for i in range(x1):
     x.append(int(input("enter number no "+ str(i+1))))
+
 def bubbleSort(alist):
+    count = 0
     for passnum in range(len(alist)-1,0,-1):
+
         for i in range(passnum):
+
             if alist[i]>alist[i+1]:
+
                 temp=alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1]=temp
+                count = count+1
+                print(str(count) + ". " + str(x))
 bubbleSort(x)
 print(x)
